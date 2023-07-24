b3d0b9d72b589ac9539feaf06a9d4770bcdaa77e
Brandon Wright <psybw7@nottingham.ac.uk>
2023-07-24 02:20:24 +0100

    Add: implement removeFromCollection

--- scripts/kluvo2.js
+++ scripts/kluvo2.js
@@ -94,7 +94,16 @@ setTimeout(function(){
     // Bind "Remove" button
     jQuery('button#ASCM_removeall').click(function(){
         jQuery('div#MySubscribedItems div.itemChoice.inCollection').each(function(){
-            window.RemoveChildFromCollection(jQuery(this).attr('id').replace('choice_MySubscribedItems_',''))
+            var collection_name = jQuery('div.manageCollectionHeader div.breadcrumbs a').eq(2).text().trim();
+            var url = new URL(document.location.href);
+            var collection_id = url.searchParams.get('id');
+            var data = {
+                id: collection_id,
+                sessionid: cookie,
+                childid: jQuery(this).attr('id').replace('choice_MySubscribedItems_',''),
+                activeSection: collection_name
+            };
+            removeFromCollection(data, jQuery(this))
         });
     });
     // Function to send a request to add item to a collection
@@ -110,6 +119,21 @@ setTimeout(function(){
             }
         });
     }
+
+    function removeFromCollection(data, object){
+    jQuery.ajax({
+        type: "POST",
+        url: 'https://steamcommunity.com/sharedfiles/removechild',
+        data: data,
+        success: function(response){
+            if(object && response.success == 1){
+                // object.addClass('inCollection');
+            }
+        }
+    });
+    }
+
+
 }, 0);
 
 
